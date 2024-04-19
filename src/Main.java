8dda27f7de2c04799416927256533ebbede0578b
VasiliySiniavin <vasinyavin@yandex.ru>
2024-04-19 20:28:47 +0300

    create LogEntry.class, Statistics.class, UserAgent.class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
+
 public class Main {
+
     public static void main(String[] args) {
         int count = 0;
         while (true) {
@@ -23,34 +26,62 @@ public class Main {
             System.out.println("Это файл номер " + count);
             int lines = 0;
             int max = 0;
-            int min = 0;
+            int countYB = 0;
+            int countGB = 0;
+            int countM = 0;
+
             try {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader = new BufferedReader(fileReader);
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    lines++;
-                    int length = line.length();
-                    if (length > max)
-                        max = length;
-                    if (length < min)
-                        min = length;
+
+                    String[] userAgent = line.split("[\"]");
+                    for (String firstBrackets : userAgent) {
+                        if (firstBrackets.contains("Mozilla")) {
+                            countM++;
+                        }
+                        String[] parts = firstBrackets.split(";");
+                        if (parts.length >= 2) {
+                            String fragment = parts[1];
+                            String[] parts1 = fragment.trim().split("\\s*(\\s|,|!|/|\\.)\\s*");
+                            String total = parts1[0];
+                            System.out.println(total);
+                            if (total.equals("YandexBot")) {
+                                countYB++;
+                            }
+                            if (total.equals("Googlebot"))
+                                countGB++;
+                        }
+                    }
                 }
+                System.out.println("Количество запросов от YandexBot: " + countYB + " или " + percentage(countYB, lines) + "%");
+                System.out.println("Количество запросов от GoogleBot: " + countGB + " или " + percentage(countGB, lines) + "%");
+                System.out.println("Количество запросов: " + countM);
                 System.out.println("Общее количество строк в файле: " + lines);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
+
             if (max > 1024) {
                 throw new exceedingCharacters("в файле встретилась строка длиннее 1024 символов");
             }
-            System.out.println("Длина самой длинной строки в файле: " + max);
-            System.out.println("Длина самой короткой строки в файле: " + min);
         }
+        }
+
+    public static double percentage(double x, double y) {
+        return Math.round((x / y*100));
+
     }
 }
-
+enum HttpMethod {
+    GET, POST, PUT, PATCH, DELETE;
+}
 class exceedingCharacters extends RuntimeException {
     public exceedingCharacters(String message) {
         super(message);
     }
 }
+
+
+
