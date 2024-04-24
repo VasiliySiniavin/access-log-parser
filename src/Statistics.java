6c8774032590d988b324ffbfd2e77979cb72b64a
VasiliySiniavin <vasinyavin@yandex.ru>
2024-04-24 19:02:23 +0300

    add method addEntry which accpets String typeSystem and method getTypeSysCount

--- src/Statistics.java
+++ src/Statistics.java
@@ -7,12 +7,31 @@ public class Statistics {
       private LocalDateTime minTime;
       private LocalDateTime maxTime;
       private HashSet<LogEntry> listPages;
+      private HashSet<String> existPages = new HashSet<>();
+      private HashMap<String, Integer> typeSysCount = new HashMap<>();
 
       public Statistics() {
-            this.totalTraffic = 0;
+          this.totalTraffic = 0;
             this.minTime = null;
             this.maxTime = null;
             this.listPages = new HashSet<>();
+      }
+      public HashMap<String, Double> getTypeSysCount() {
+            HashMap<String, Double> sysStatistic = new HashMap<>();
+            int totalSys = 0;
+            for (int count :  typeSysCount.values()) {
+                  totalSys += count;
+            }
+            for (String sys : typeSysCount.keySet()) {
+                  int count  = typeSysCount.get(sys);
+                          double fraction = (double) count / totalSys;
+                  sysStatistic.put(sys, fraction);
+            }
+            return sysStatistic;
+      }
+
+      public HashSet<String> getExistPages() {
+            return existPages;
       }
 
       public void addEntry(LogEntry logEntry) {
@@ -25,7 +44,18 @@ public class Statistics {
             if (this.maxTime == null || logEntryTime.isBefore(maxTime)) {
                   maxTime = logEntryTime;
             }
-            listPages.add(logEntry);
+            if (logEntry.getResponseCode() == 200) {
+                  existPages.add(logEntry.path);
+            }
+          listPages.add(logEntry);
+      }
+      public void addEntry(String sys) {
+            if (typeSysCount.containsKey(sys)) {
+                  int count = typeSysCount.get(sys);
+                  typeSysCount.put(sys, count + 1);
+            } else {
+                  typeSysCount.put(sys, 1);
+            }
       }
 
       public int getTrafficRate() {
