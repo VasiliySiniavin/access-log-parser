b9c60d5d4a4abd707b9c50e25aa9101b0d5abb2c
VasiliySiniavin <vasinyavin@yandex.ru>
2024-04-26 09:32:20 +0300

    delete addEntry which accepts String typeSystem and add method getBrowserCount and getNonExistPages and add in addEntry code which search TypeSystems and browsers

--- src/Statistics.java
+++ src/Statistics.java
@@ -8,8 +8,11 @@ public class Statistics {
       private LocalDateTime maxTime;
       private HashSet<LogEntry> listPages;
       private HashSet<String> existPages = new HashSet<>();
+      private HashSet<String> nonExistPages = new HashSet<>();
       private HashMap<String, Integer> typeSysCount = new HashMap<>();
+      private HashMap<String, Integer> browserCount = new HashMap<>();
 
+      UserAgent userAgent;
       public Statistics() {
           this.totalTraffic = 0;
             this.minTime = null;
@@ -30,8 +33,26 @@ public class Statistics {
             return sysStatistic;
       }
 
+      public HashMap<String, Double> getBrowserCountt() {
+            HashMap<String, Double> browserStatistic = new HashMap<>();
+            int totalBrowser = 0;
+            for (int count :  browserCount.values()) {
+                  totalBrowser += count;
+            }
+            for (String browser : typeSysCount.keySet()) {
+                  int count  = typeSysCount.get(browser);
+                  double fraction = (double) count / totalBrowser;
+                  browserStatistic.put(browser, fraction);
+            }
+            return browserStatistic;
+      }
+
       public HashSet<String> getExistPages() {
             return existPages;
+      }
+
+      public HashSet<String> getNonExistPages() {
+            return nonExistPages;
       }
 
       public void addEntry(LogEntry logEntry) {
@@ -47,15 +68,13 @@ public class Statistics {
             if (logEntry.getResponseCode() == 200) {
                   existPages.add(logEntry.path);
             }
-          listPages.add(logEntry);
-      }
-      public void addEntry(String sys) {
-            if (typeSysCount.containsKey(sys)) {
-                  int count = typeSysCount.get(sys);
-                  typeSysCount.put(sys, count + 1);
-            } else {
-                  typeSysCount.put(sys, 1);
+            if (logEntry.getResponseCode() == 404) {
+                  nonExistPages.add(logEntry.path);
             }
+            typeSysCount.put(String.valueOf(userAgent.getTypeSys()), typeSysCount.getOrDefault(userAgent.getTypeSys(), 0) + 1);
+            browserCount.put(String.valueOf(userAgent.getBrowser()), browserCount.getOrDefault(userAgent.getBrowser(), 0) + 1);
+
+          listPages.add(logEntry);
       }
 
       public int getTrafficRate() {
