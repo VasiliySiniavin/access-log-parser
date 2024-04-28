6556b5e95809e0f56349bdcb74ffe2179ca8d0c4
VasiliySiniavin <vasinyavin@yandex.ru>
2024-04-28 12:46:02 +0300

    add methods isBot, getAvgTotalVisitPerHour, getTotalErrorPerHour, getAvgVisitPerUser

--- src/Statistics.java
+++ src/Statistics.java
@@ -12,6 +12,11 @@ public class Statistics {
       private HashMap<String, Integer> typeSysCount = new HashMap<>();
       private HashMap<String, Integer> browserCount = new HashMap<>();
 
+      private int totalVisit = 0;
+      private int totalError = 0;
+      private Map<String, Integer> userVisit = new HashMap<>();
+      private Set<String> bots = new HashSet<>();
+
       UserAgent userAgent;
       public Statistics() {
           this.totalTraffic = 0;
@@ -58,6 +63,7 @@ public class Statistics {
       public void addEntry(LogEntry logEntry) {
             totalTraffic += logEntry.getDataSize();
             LocalDateTime logEntryTime = logEntry.getDateTime();
+            totalVisit++;
 
             if (this.minTime == null || logEntryTime.isBefore(minTime)) {
                   minTime = logEntryTime;
@@ -71,10 +77,37 @@ public class Statistics {
             if (logEntry.getResponseCode() == 404) {
                   nonExistPages.add(logEntry.path);
             }
+
+            if(logEntry.getResponseCode() >= 400 && logEntry.getResponseCode() < 600) {
+                  totalError++;
+            }
+            if(!isBot(userAgent)) {
+                  userVisit.put(logEntry.ipAddress, userVisit.getOrDefault(logEntry.ipAddress, 0) + 1);
+            }
             typeSysCount.put(String.valueOf(userAgent.getTypeSys()), typeSysCount.getOrDefault(userAgent.getTypeSys(), 0) + 1);
             browserCount.put(String.valueOf(userAgent.getBrowser()), browserCount.getOrDefault(userAgent.getBrowser(), 0) + 1);
 
           listPages.add(logEntry);
+      }
+
+      public boolean isBot(UserAgent userAgent) {
+            return userAgent.toString().contains("bot");
+      }
+      public double getAvgTotalVisitPerHour(double hours) {
+            return totalVisit / hours;
+      }
+
+      public double getTotalErrorPerHour(double hours) {
+            return totalError / hours;
+      }
+
+      public double getAvgVisitPerUser() {
+            if(userVisit.isEmpty()) {
+                  return 0;
+            }
+            int totalUsers = userVisit.size();
+            int totalUserVisit = userVisit.values().stream().mapToInt(Integer::intValue).sum();
+            return totalUserVisit / totalUsers;
       }
 
       public int getTrafficRate() {
